166502eb08af3467118d324658746e5a372b2d02
John Tyrone Pagunsan Coronel <jtcoronel.chmsu@gmail.com>
2026-03-17 20:44:13 +0800

    .

--- student_application/settings.py
+++ student_application/settings.py
@@ -36,7 +36,7 @@ CSRF_TRUSTED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
-    "jet",
+    # "jet",
     "home",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -79,10 +79,20 @@ WSGI_APPLICATION = "student_application.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'student_assistant_db',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',  # Use 'localhost' for local development
+        'PORT': '5432',       # The default PostgreSQL port
     }
 }
 
