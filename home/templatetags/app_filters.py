df0a4f06e9e2b220189ee27e31100513899638f1
John Tyrone Pagunsan Coronel <jtcoronel.chmsu@gmail.com>
2026-03-24 12:17:43 +0800

    remove comments

--- home/templatetags/app_filters.py
+++ home/templatetags/app_filters.py
@@ -5,10 +5,7 @@ register = template.Library()
 
 @register.filter
 def get_item(dictionary, key):
-    """Retrieve a value from a dict by key in templates.
 
-    Usage: {{ mydict|get_item:key_var }}
-    """
     if isinstance(dictionary, dict):
         return dictionary.get(key, [])
     return []
